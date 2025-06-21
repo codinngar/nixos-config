c3b235f06d36eb60070a0571bf1fbf0eeb1c0c8a
codinngar <codinngar@gmail.com>
2025-06-21 22:08:43 +0300

    Add hyprland and waybar

--- configuration.nix
+++ configuration.nix
@@ -51,9 +51,17 @@
   };
 
   # Desktop
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
   # Keymap
   services.xserver.xkb = {
