48b8510103bbeb62a3c9dc526ade33a03911da19
codinngar <codinngar@gmail.com>
2025-06-22 01:12:46 +0300

    Add hyprpaper

---
+++ modules/hyprpaper.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [ "./assets/wallpaper.jpg" ];
+      wallpaper = [ ", ./assets/wallpaper.jpg" ];
+    };
+  };
+}
