27ebd0a83d687f4db8c996ff537ae56f875212cf
codinngar <codinngar@gmail.com>
2025-06-22 16:27:55 +0300

    Add vscode ext

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -6,6 +6,9 @@
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
         vscodevim.vim
+        dbaeumer.vscode-eslint
+        esbenp.prettier-vscode
+        christian-kohler.path-intellisense
       ];
     };
   };
