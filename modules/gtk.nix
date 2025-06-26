329bab994dbd544e896a0861dbb7d04a61cf4d4d
codinngar <codinngar@gmail.com>
2025-06-26 23:43:49 +0300

    Change icon theme to whitesur

--- modules/gtk.nix
+++ modules/gtk.nix
@@ -3,6 +3,10 @@
 {
   gtk = {
     enable = true;
+    iconTheme = {
+      name = "WhiteSur";
+      package = pkgs.whitesur-icon-theme;
+    };
     cursorTheme = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
